f006b5553945779c1d2333103081943e52298650
musyafaa <musafadila2132@gmail.com>
2022-01-19 14:59:55 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/about', function (){
+    return view('about', [
+        "nama" => "Muhammad Musyafa Fadila",
+        "email" => "musafadila2132@gmail.com",
+        "gambar" => "foto ujian.jpg"
+    ]);
+});
+
+Route::get('/gallery', function (){
+    return view('gallery');
+});
